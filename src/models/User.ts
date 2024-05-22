e3a9fd839c81e8086cd4498c9dbac22aa56a129f
Isabel Rico <marissa.rico28@gmail.com>
2024-05-22 11:26:15 +0200

    ending first models

---
+++ src/models/User.ts
@@ -0,0 +1,27 @@
+import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm"
+import { Role } from "./Role";
+
+@Entity("users")
+export class User extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ name: "first_name" })
+    firstName!: string;
+
+    @Column({ name: "last_name" })
+    lastName!: string;
+
+    @Column({ name: "email" })
+    email!: string;
+
+    @Column({ name: "password" })
+    password!: string;
+
+    @Column({ name: "is_active" })
+    isActive!: boolean;
+
+    @ManyToOne(() => Role, (role)=> role.users)
+    @JoinColumn({ name: "role_id"}) 
+    role!: Role;
+}
