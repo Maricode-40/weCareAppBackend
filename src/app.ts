1799aabe3de29b8939ae431acf6d1b88cb7dc22b
Isabel Rico <marissa.rico28@gmail.com>
2024-05-31 11:00:18 +0200

    finishing backend adding errorhandler middleware

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import CorsOptions from "./config/cors";
 import dotenv from "dotenv";
 import apiRoutes from "./routes/api.routes";
 import baseRoutes from "./routes/ base.routes";
+import { handleNotFound } from "./middlewares/errorHandler";
 dotenv.config();
 
 const app: Application = express();
@@ -18,4 +19,6 @@ app.use("/", baseRoutes);
 // Register API routes for resources
 app.use("/api", apiRoutes);
 
+//handle 404 erorrs 
+app.use(handleNotFound); 
 export default app;
