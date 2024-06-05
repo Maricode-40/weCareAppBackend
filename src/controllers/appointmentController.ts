f56bfb86c3ecde344001713b2ddfe8bba479489a
Isabel Rico <marissa.rico28@gmail.com>
2024-06-05 19:59:14 +0200

    extras

--- src/controllers/appointmentController.ts
+++ src/controllers/appointmentController.ts
@@ -6,9 +6,7 @@ import { Client } from "../models/Client";
 export const appointmentController = {
   async getAll(req: Request, res: Response) {
     try {
-      console.log("getAll");
-
-      const page = Number(req.query.page) || 1;
+       const page = Number(req.query.page) || 1;
       const limit = Number(req.query.limit) || 10;
 
       const [appointments, totalAppointments] = await Appointment.findAndCount({
